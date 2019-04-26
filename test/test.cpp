495f8bc52db67f5db501772be36be05c1d5bdf7f
mina <minas.gorgy@gmail.com>
2019-04-26 18:40:35 +0200

    Remove files for testing

--- test/test.cpp
+++ test/test.cpp
@@ -1,10 +1,20 @@
 #include <string>
 #include <fstream>
 #include "../build/parser.hpp"
+#include "../build/scanner.hpp"
 using namespace std;
 
 
 int main() {
+    string program =
+        "while(x < 5) {"
+            "if(x / 2 == 1) {"
+                "y = y + 1;"
+            "} else {"
+                "y = z - 3;"
+            "}"
+        "}";
+    
     string expected = 
         "L000:\n"      
             "\tpush	x\n"
@@ -31,18 +41,14 @@ int main() {
             "\tjmp	L000\n"
         "L001:\n";
 
-    if(freopen("../res/program.txt", "r", stdin) == NULL) {
-       perror("Can't open program.txt\n");
-       return EXIT_FAILURE;
-    }
-
-    int buffer_size = 2047;
+    int buffer_size = 2048;
     char output[buffer_size];
     FILE *f = freopen("/dev/null", "w", stdout);
     setvbuf(stdout, output, _IOFBF, buffer_size);
 
+    yy_scan_string(program.c_str());
     yyparse();
     fclose(f);
 
-    return expected.compare(output);
+    return expected.compare(string(output));
 }
