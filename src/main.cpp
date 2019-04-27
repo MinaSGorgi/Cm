7bf1ac6b102331843ea071546d3b78657e8e3a6f
mina <minas.gorgy@gmail.com>
2019-04-27 15:42:56 +0200

    src/main.cpp: added compiling files

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,19 @@
+#include <fstream>
 #include "../build/parser.hpp"
+#include "../build/scanner.hpp"
 
 
-int main(void) {
-    yyparse();
+int main(int argc, char **argv) {
+
+    if(argc > 1) {
+        ifstream file(argv[1]);
+        string program((istreambuf_iterator<char>(file)), istreambuf_iterator<char>());
+        yy_scan_string(program.c_str());
+        yyparse();
+        file.close();
+    } else {
+        yyparse();
+    }
+
     return 0;
 } 
