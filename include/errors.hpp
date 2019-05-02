f4ec12441c79f049e44a269cf464bdf50c6d9ffe
mina <minas.gorgy@gmail.com>
2019-05-02 05:07:13 +0200

    include/errors.hpp: added TypeMismatch and fixed empty error messages

--- include/errors.hpp
+++ include/errors.hpp
@@ -1,53 +1,42 @@
 #ifndef ERRORS_H
 #define ERRORS_H
 
-#include <exception>
+#include <stdexcept>
 #include <string>
 using namespace std;
 
 
-class UndefinedReference: public exception {
-    const string symbol;
-
-public:
-    UndefinedReference(const string& symbol): symbol(symbol) {}
-
-    virtual const char* what() const throw() {
-        return ("Undefined reference to: " + symbol).c_str();
-    }
+class UndefinedReference: public runtime_error {
+    public:
+        UndefinedReference(const string& symbol): runtime_error("Undefined reference to: " + symbol)
+            { }
 };
 
-class MultipleDefinition: public exception {
-    const string symbol;
-
-public:
-    MultipleDefinition(const string& symbol): symbol(symbol) {}
-
-    virtual const char* what() const throw() {
-        return ("Multiple Definition of: " + symbol).c_str();
-    }
+class MultipleDefinition: public runtime_error {
+    public:
+        MultipleDefinition(const string& symbol): runtime_error("Multiple Definition of: " + symbol)
+            { }
 };
 
-class ReadOnly: public exception {
-    const string symbol;
-
-public:
-    ReadOnly(const string& symbol): symbol(symbol) {}
-
-    virtual const char* what() const throw() {
-        return ("Assignment of read-only variable: " + symbol).c_str();
-    }
+class ReadOnly: public runtime_error {
+    public:
+        ReadOnly(const string& symbol): runtime_error("Assignment of read-only variable: " + symbol)
+            { }
 };
 
-class Uninitialized: public exception {
-    const string symbol;
+class Uninitialized: public runtime_error {
 
-public:
-    Uninitialized(const string& symbol): symbol(symbol) {}
+    public:
+        Uninitialized(const string& symbol):
+            runtime_error("The variable `" + symbol + "` is being used without being initialized")
+            { }
+};
 
-    virtual const char* what() const throw() {
-        return ("The variable: " + symbol + " is being used without being initialized").c_str();
-    }
+class TypeMismatch: public runtime_error {
+    public:
+        TypeMismatch(const string& symbol1, const string& symbol2, const string& type1,
+            const string& type2): runtime_error("Type `" + type1 + "` of variable `" + symbol1 +
+                "` doesn't match Type `" + type2 + "` of variable `" + symbol2 + "`") { }
 };
 
 #endif /* ERRORS_H */
